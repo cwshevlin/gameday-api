0aa67fb88844f180be3d41b5ab9f6bb050dc3387
cwshevlin <cwshevlin@gmail.com>
2015-05-22 21:34:26 -0400

    Refactored the parse_innings_ab_data.

--- lib/gameday_api/gameday_parser.rb
+++ lib/gameday_api/gameday_parser.rb
@@ -143,86 +143,97 @@ module GamedayApi
   		current_order=0
   		inning = doc.root.attributes['num']
   		doc.root.each_element do |inning_half|
-  			begin
-  				half = inning_half.name
-  				inning_half.each_element do |atbat_action|
-  					current_order = current_order+1
-  					if atbat_action.name == 'atbat'
-  						batter = Player.find(:first, :conditions=>'gameday_id='+atbat_action.attributes['batter'])
-  						pitcher = Player.find(:first, :conditions=>'gameday_id='+atbat_action.attributes['pitcher'])
-  						atbat = AtBat.new
-  						atbat.inning = inning
-  						atbat.half = half
-  						atbat.number = atbat_action.attributes['num']
-  						current_atbat = atbat.number
-  						atbat.ball = atbat_action.attributes['b']
-  						atbat.strike = atbat_action.attributes['s']
-  						atbat.out = atbat_action.attributes['o']
-  						atbat.batter_gd_id = atbat_action.attributes['batter']
-  						atbat.pitcher_gd_id = atbat_action.attributes['pitcher']
-  						atbat.stand = atbat_action.attributes['stand']
-  						atbat.description = atbat_action.attributes['des']
-  						atbat.event = atbat_action.attributes['event']
-  						if batter
-  							atbat.batter_id = batter.id
-  						end
-  						if pitcher
-  							atbat.pitcher_id = pitcher.id
-  						end
-  						atbat.game_id = game_id
-  						atbat.order = current_order
-  						atbat.gd_type = 'atbat'
-  						atbat.save
-  						atbat_action.each_element do |pitch_runner|
-  							if pitch_runner.name == 'pitch'
-  								pitch = Pitch.new
-  								pitch.at_bat_id = atbat.id
-  								pitch.description = pitch_runner.attributes['des']	
-  								pitch.pitch_type = pitch_runner.attributes['type']
-  								pitch.gd_pitch_id = pitch_runner.attributes['id']
-  								pitch.x = pitch_runner.attributes['x']
-  								pitch.y = pitch_runner.attributes['y']
-  								pitch.on_1b = pitch_runner.attributes['on_1b']
-  								pitch.on_2b = pitch_runner.attributes['on_2b']
-  								pitch.on_3b = pitch_runner.attributes['on_3b']
-  								pitch.save
-  							elsif pitch_runner.name == 'runner'
-  								runner = Runner.new
-  								player = Player.find(:first, :conditions=>"gameday_id=#{pitch_runner.attributes['id']}")
-  								runner.player_id = player.id
-  								runner.start = pitch_runner.attributes['start']
-  								runner.end = pitch_runner.attributes['end']
-  								runner.event = pitch_runner.attributes['event']
-  								runner.score = pitch_runner.attributes['score']
-  								runner.rbi = pitch_runner.attributes['rbi']
-  								runner.earned = pitch_runner.attributes['earned']
-  								runner.at_bat_id = atbat.id
-  								runner.save
-  							end
-  						end
-  					elsif atbat_action.name == 'action'
-  						action = AtBat.new
-  						action.ball = atbat_action.attributes['b']
-  						action.strike = atbat_action.attributes['s']
-  						action.out = atbat_action.attributes['o']
-  						action.description = atbat_action.attributes['des']
-  						action.event = atbat_action.attributes['event']
-  						action.pitch = atbat_action.attributes['pitch']
-  						action.score = atbat_action.attributes['score']
-  						action.game_id = game_id					
-  						player = Player.find(:first, :conditions=>"gameday_id=#{atbat_action.attributes['player']}")
-  						action.player_id = player.id
-  						action.inning = inning
-  						action.half = half
-  						action.order = current_order
-  						action.gd_type = 'action'
-  						# action.next_atbat = current_atbat + 1
-  						action.save
-  					end
-  				end
-  			end
-  		end
-  	end
+				half = inning_half.name
+				inning_half.each_element do |atbat_action|
+					current_order += 1
+					if atbat_action.name == 'atbat'
+						batter = Player.find(:first, :conditions=>'gameday_id='+atbat_action.attributes['batter'])
+						pitcher = Player.find(:first, :conditions=>'gameday_id='+atbat_action.attributes['pitcher'])
+            atbat = set_atbat_from_at_bat_action(atbat_action)
+            atbat.inning = inning
+            atbat.half = half
+            current_atbat = atbat.number
+						atbat.batter_id = batter.id if batter
+						atbat.pitcher_id = pitcher.id if pitcher
+						atbat.game_id = game_id
+						atbat.order = current_order
+						atbat.gd_type = 'atbat'
+						atbat.save
+						atbat_action.each_element do |pitch_runner|
+							if pitch_runner.name == 'pitch'
+								pitch = set_pitch_from_pitch_runner(pitch_runner)
+								pitch.at_bat_id = atbat.id
+								pitch.save
+							elsif pitch_runner.name == 'runner'
+                player = Player.find(:first, :conditions=>"gameday_id=#{pitch_runner.attributes['id']}")
+                runner = set_runner_from_pitch_runner(pitch_runner)
+								runner.player_id = player.id
+								runner.at_bat_id = atbat.id
+								runner.save
+							end
+						end
+					elsif atbat_action.name == 'action'
+						action = set_action_from_atbat_action(atbat_action)
+						action.game_id = game_id					
+						player = Player.find(:first, :conditions=>"gameday_id=#{atbat_action.attributes['player']}")
+						action.player_id = player.id
+						action.inning = inning
+						action.half = half
+						action.order = current_order
+						action.gd_type = 'action'
+						action.save
+					end
+				end
+			end
+		end
+
+# TODO: Check to see if Ruby can handle this end in here. I'm thinking it' going to be reserved,
+# but I can't imagine that it was working before.
+    def self.set_runner_from_pitch_runner(pitch_runner)
+      runner = Runner.new
+      runner.start = pitch_runner.attributes['start']
+      runner.end = pitch_runner.attributes['end']
+      runner.event = pitch_runner.attributes['event']
+      runner.score = pitch_runner.attributes['score']
+      runner.rbi = pitch_runner.attributes['rbi']
+      runner.earned = pitch_runner.attributes['earned']
+    end
+
+    def self.set_pitch_from_pitch_runner(pitch_runner)
+      pitch = Pitch.new
+      pitch.description = pitch_runner.attributes['des']  
+      pitch.pitch_type = pitch_runner.attributes['type']
+      pitch.gd_pitch_id = pitch_runner.attributes['id']
+      pitch.x = pitch_runner.attributes['x']
+      pitch.y = pitch_runner.attributes['y']
+      pitch.on_1b = pitch_runner.attributes['on_1b']
+      pitch.on_2b = pitch_runner.attributes['on_2b']
+      pitch.on_3b = pitch_runner.attributes['on_3b']
+    end
+
+    def self.set_atbat_from_atbat_action(atbat_action)
+      atbat = AtBat.new
+      atbat.number = atbat_action.attributes['num']
+      atbat.ball = atbat_action.attributes['b']
+      atbat.strike = atbat_action.attributes['s']
+      atbat.out = atbat_action.attributes['o']
+      atbat.batter_gd_id = atbat_action.attributes['batter']
+      atbat.pitcher_gd_id = atbat_action.attributes['pitcher']
+      atbat.stand = atbat_action.attributes['stand']
+      atbat.description = atbat_action.attributes['des']
+      atbat.event = atbat_action.attributes['event']
+    end
+
+    def self.set_action_from_atbat_action(atbat_action)
+      action = AtBat.new
+      action.ball = atbat_action.attributes['b']
+      action.strike = atbat_action.attributes['s']
+      action.out = atbat_action.attributes['o']
+      action.description = atbat_action.attributes['des']
+      action.event = atbat_action.attributes['event']
+      action.pitch = atbat_action.attributes['pitch']
+      action.score = atbat_action.attributes['score']
+    end
 	
 	
   	def self.parse_event_log_data(doc, game_id)
